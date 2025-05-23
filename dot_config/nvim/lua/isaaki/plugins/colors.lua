beb86f3beac5f1bc727cd1deb22122c8acebda90
isaaki <xisaaki@proton.me>
2025-05-23 18:05:16 +0200

    add transparecy to nvim

--- dot_config/nvim/lua/isaaki/plugins/colors.lua
+++ dot_config/nvim/lua/isaaki/plugins/colors.lua
@@ -7,14 +7,29 @@
 -- }
 
 return {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      style = "day"
-    },
-    -- config = function()
-    --   -- load the colorscheme here
-    --   vim.cmd([[colorscheme tokyonight-night]])
-    -- end,
+	"folke/tokyonight.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	opts = {
+		cache = false,
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
+	config = function()
+		-- load the colorscheme here
+		vim.cmd([[colorscheme tokyonight-night]])
+
+    -- Set background transparent
+		vim.cmd([[
+      highlight Normal guibg=NONE ctermbg=NONE
+      highlight NormalNC guibg=NONE ctermbg=NONE
+      highlight SignColumn guibg=NONE ctermbg=NONE
+      highlight VertSplit guibg=NONE ctermbg=NONE
+      highlight StatusLine guibg=NONE ctermbg=NONE
+      highlight EndOfBuffer guibg=NONE ctermbg=NONE
+    ]])
+	end,
 }
