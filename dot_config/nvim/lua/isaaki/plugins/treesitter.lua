8bc2e436c1171ca816a7fbba7f1fb7b2871177a7
isaaki <xisaaki@proton.me>
2026-01-29 20:56:10 +0100

    update treesitter config

--- dot_config/nvim/lua/isaaki/plugins/treesitter.lua
+++ dot_config/nvim/lua/isaaki/plugins/treesitter.lua
@@ -1,39 +1,50 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		{ "JoosepAlviste/nvim-ts-context-commentstring" },
-		{ "nvim-treesitter/nvim-treesitter-context" },
-		{ "nvim-treesitter/nvim-treesitter-textobjects" },
-	},
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
-	end,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all"
-			ensure_installed = { "javascript", "typescript", "vue", "c", "rust", "lua", "vim", "vimdoc", "zig" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      { "JoosepAlviste/nvim-ts-context-commentstring" },
+      { "nvim-treesitter/nvim-treesitter-textobjects" },
+    },
+    event = "BufRead",
+    build = ":TSUpdate",
+    ---@class TSConfig
+    opts = {
+      ensure_installed = {
+        "javascript", "typescript", "vue", "c", "rust", "lua", "vim", "vimdoc", "zig" 
+      },
+    },
+    config = function()
+      -- install parsers from custom opts.ensure_installed
+      if opts.ensure_installed and #opts.ensure_installed > 0 then
+        require("nvim-treesitter").install(opts.ensure_installed)
+        -- register and start parsers for filetypes
+        for _, parser in ipairs(opts.ensure_installed) do
+          local filetypes = parser -- In this case, parser is the filetype/language name
+          vim.treesitter.language.register(parser, filetypes)
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = true,
+          vim.api.nvim_create_autocmd({ "FileType" }, {
+            pattern = filetypes,
+            callback = function(event)
+              vim.treesitter.start(event.buf, parser)
+            end,
+          })
+        end
+      end
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = false,
-
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = not vim.g.vscode,
-
-				additional_vim_regex_highlighting = false,
-			},
-		})
-
-		require("nvim-treesitter.configs").setup({})
-
-		if not vim.g.vscode then
-			require("treesitter-context").setup({
-				trim_scope = "inner",
-			})
-		end
-	end,
+      require("treesitter-context").setup({
+        trim_scope = "inner",
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "BufRead",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      event = "BufRead",
+    },
+    opts = {
+      multiwindow = true,
+    },
+  },
 }
