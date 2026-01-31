9fb1153e7ad6bf065b3a540f04b7a0a99d55c966
isaaki <xisaaki@proton.me>
2026-01-31 11:41:06 +0100

    fix nvim treesitter config

--- dot_config/nvim/lua/isaaki/plugins/treesitter.lua
+++ dot_config/nvim/lua/isaaki/plugins/treesitter.lua
@@ -1,50 +1,38 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { "JoosepAlviste/nvim-ts-context-commentstring" },
-      { "nvim-treesitter/nvim-treesitter-textobjects" },
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "JoosepAlviste/nvim-ts-context-commentstring" },
+    { "nvim-treesitter/nvim-treesitter-context" }
+  },
+  event = "BufRead",
+  build = ":TSUpdate",
+  ---@class TSConfig
+  opts = {
+    ensure_installed = {
+      "javascript", "typescript", "vue", "c", "rust", "lua", "vim", "vimdoc", "zig" 
     },
-    event = "BufRead",
-    build = ":TSUpdate",
-    ---@class TSConfig
-    opts = {
-      ensure_installed = {
-        "javascript", "typescript", "vue", "c", "rust", "lua", "vim", "vimdoc", "zig" 
-      },
-    },
-    config = function()
-      -- install parsers from custom opts.ensure_installed
-      if opts.ensure_installed and #opts.ensure_installed > 0 then
-        require("nvim-treesitter").install(opts.ensure_installed)
-        -- register and start parsers for filetypes
-        for _, parser in ipairs(opts.ensure_installed) do
-          local filetypes = parser -- In this case, parser is the filetype/language name
-          vim.treesitter.language.register(parser, filetypes)
+  },
+  config = function(_, opts)
+    -- install parsers from custom opts.ensure_installed
+    if opts.ensure_installed and #opts.ensure_installed > 0 then
+      require("nvim-treesitter").install(opts.ensure_installed)
+      -- register and start parsers for filetypes
+      for _, parser in ipairs(opts.ensure_installed) do
+        local filetypes = parser -- In this case, parser is the filetype/language name
+        vim.treesitter.language.register(parser, filetypes)
 
-          vim.api.nvim_create_autocmd({ "FileType" }, {
-            pattern = filetypes,
-            callback = function(event)
-              vim.treesitter.start(event.buf, parser)
-            end,
-          })
-        end
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+          pattern = filetypes,
+          callback = function(event)
+            vim.treesitter.start(event.buf, parser)
+          end,
+        })
       end
+    end
 
-      require("treesitter-context").setup({
-        trim_scope = "inner",
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "BufRead",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      event = "BufRead",
-    },
-    opts = {
-      multiwindow = true,
-    },
-  },
+    require("treesitter-context").setup({
+      trim_scope = "inner",
+    })
+  end,
 }
+
