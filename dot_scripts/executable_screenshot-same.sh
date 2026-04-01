3d5e727e1f982969278a17ec17ba435c147ac8ba
isaaki <xisaaki@proton.me>
2026-04-01 13:02:17 +0200

    add new screenshot tool bindings niri

---
+++ dot_scripts/executable_screenshot-same.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+mkdir -p "$HOME/Pictures/satty"
+grim -t ppm -g "$(tr -d '\n' < /tmp/last-slurp)" - | "$HOME/.local/bin/satty" \
+    --filename - \
+    --initial-tool arrow \
+    --copy-command wl-copy \
+    --brush-smooth-history-size 5 \
+    --disable-notifications \
+    --actions-on-escape "save-to-clipboard,exit" \
+    --output-filename "$HOME/Pictures/satty/$(date '+%Y-%m-%d_%H-%M-%S').png"
