9ea0a4f05db9cdb07a9a960073aece9f75763b73
isaaki <xisaaki@proton.me>
2026-08-25 10:15:57 +0200

    update configs

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -59,3 +59,5 @@ end
 set -gx PATH $PATH /home/isaaki/.lmstudio/bin
 # End of LM Studio CLI section
 
+# Mise activation
+/home/isak/.local/bin/mise activate fish | source # added by https://mise.run/fish
